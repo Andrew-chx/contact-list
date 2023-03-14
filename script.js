d6868b263de86bdb5e6f4860049ad9f639434961
Andres <adochapi2000@gmail.com>
2023-03-14 18:06:55 -0500

    Añado todos los archivos a github

---
+++ script.js
@@ -0,0 +1,16 @@
+let contactos = [
+  {
+    nombre: "Andres", telefono: "323 247 28 51",
+  },
+  {
+    nombre: "Mamá", telefono: "313 000 00 00",
+  },
+  {
+    nombre: "Pedro", telefono: "311 111 11 11",
+  },
+  {
+    nombre: "Kathe", telefono: "321 444 44 44",
+  },
+];
+
+console.log(contactos);
